19774705fb62030c58d2ec95649beeb8c8b26dbb
fgg1987 <134975346+fgg1987@users.noreply.github.com>
2024-02-15 22:31:59 +0100

    Update setup.sh

--- pipelines/PIPELINE-FULL-STAGING/setup.sh
+++ pipelines/PIPELINE-FULL-STAGING/setup.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
 set -x
-python3.7 -m venv todo-list-aws
+python3 -m venv todo-list-aws
 source todo-list-aws/bin/activate
-python -m pip install --upgrade pip
+python3 -m pip install --upgrade pip
 #For static testing
-python -m pip install radon
-python -m pip install flake8
-python -m pip install flake8-polyfill
-python -m pip install bandit
+python3 -m pip install radon
+python3 -m pip install flake8
+python3 -m pip install flake8-polyfill
+python3 -m pip install bandit
 #For integration testing
-python -m pip install pytest
+python3 -m pip install pytest
 #For unit testing
-python -m pip install boto3
-python -m pip install moto
-python -m pip install mock==4.0.2
-python -m pip install coverage==4.5.4
-
+python3 -m pip install boto3
+python3 -m pip install moto
+python3 -m pip install mock==4.0.2
+python3 -m pip install coverage==4.5.4
+python3 -m pip install urllib3==1.26.6
+# For HTTP requests
+python3 -m pip install requests
 
 pwd
